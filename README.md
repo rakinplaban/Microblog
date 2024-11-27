4309784b4e42051a62a6dae4da822727d9841fef
rakinplaban <rakinshahriar54@gmail.com>
2024-11-27 23:43:20 +0600

    Initialiged repo.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Flask Learning Journey 🚀
+Welcome to my Flask learning repository! Here's what I’ve achieved so far:
+- Rendered my first HTML template using Flask's `render_template()` function. ✅
+- Next milestone: Add routes for handling dynamic content. 🛠️
+
+## Updates
+- **Date**: Nov 27, 2024
+- **Achievement**: Learned how to render templates using Flask! 😊
---
+++ flaskblog.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = (
+    {
+        'author': 'Hatsuki',
+        'title': 'Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+     {
+        'author': 'Hatsuki',
+        'title': 'Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+)
+
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def hello():
+    str = "Hatsuki"
+    return render_template('index.html', title='flask test', name=str,
+                           posts=posts)
+
+@app.route('/about')
+def about():
+    return 'About'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
